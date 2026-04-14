82cc380494f2f8f49fb52519877406da2e500b87
Mario <mariomartinezferreiro@gmail.com>
2026-04-14 19:29:53 +0200

    Clase pieza
    Constructor, getters y setters

--- src/src/piezas/Pieza.java
+++ src/src/piezas/Pieza.java
@@ -1,5 +1,39 @@
 package piezas;
 
-public abstract class Pieza {
-    
+import interfaces.Movible;
+
+public abstract class Pieza implements Movible {
+    private String color;
+    private int filaActual;
+    private int columnaActual;
+
+    public Pieza(String color, int filaActual, int columnaActual){
+        this.color = color;
+        this.filaActual = filaActual;
+        this.columnaActual = columnaActual;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public int getFilaActual(){
+        return this.filaActual;
+    }
+
+    public int getColumnaActual(){
+        return this.columnaActual;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public void setFilaActual(int filaActual){
+        this.filaActual = filaActual;
+    }
+
+    public void setColumnaActual(int columnaActual){
+        this.columnaActual = columnaActual;
+    }
 }
