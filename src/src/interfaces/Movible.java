dc0bc00e60194ab19fe751f73514b72c6bf0eee1
Mario <mariomartinezferreiro@gmail.com>
2026-04-14 19:17:56 +0200

    Interfaz Movible terminada

--- src/src/interfaces/Movible.java
+++ src/src/interfaces/Movible.java
@@ -3,11 +3,9 @@ package interfaces;
 import main.Tablero;
 
 public interface Movible {
-
-    public boolean mover(fila, columna, Tablero);
-
     /**
      * En esta interfaz el método mover servirá para saber a qué fila
      * y qué columna se mueve la pieza seleccionada.
      */
+    public boolean mover(int filaDestino, int columnaDestino, Tablero tablero);
 }
--- src/src/piezas/Pieza.java
+++ src/src/piezas/Pieza.java
@@ -1,5 +1,5 @@
 package piezas;
 
 public abstract class Pieza {
-
+    
 }
