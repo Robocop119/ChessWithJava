a94b56933e914e2880277b3788c600b2ca944ff8
Mario <mariomartinezferreiro@gmail.com>
2026-04-28 14:04:54 +0200

    Inicio clase Tablero

--- src/src/main/Tablero.java
+++ src/src/main/Tablero.java
@@ -1,5 +1,19 @@
 package main;
 
-public class Tablero {
+import piezas.Pieza;
 
+public class Tablero {
+    private Pieza[][] tablero;
+
+    public Tablero(Pieza[][] tablero){
+        this.tablero = new Pieza[8][8];
+    }
+
+    public Pieza[][] getTablero() {
+        return tablero;
+    }
+
+    public void setTablero(Pieza[][] tablero) {
+        this.tablero = tablero;
+    }
 }
