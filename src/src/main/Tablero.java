652f37b6b3572eedb44172d4a1b3d49b8f2bb2cd
Mario <mariomartinezferreiro@gmail.com>
2026-04-29 13:51:03 +0200

    Función mostrarTablero y correcciones

--- src/src/main/Tablero.java
+++ src/src/main/Tablero.java
@@ -1,12 +1,15 @@
 package main;
 
 import piezas.Pieza;
+import piezas.Rey;
 
-public class Tablero {
+public class Tablero{
     private Pieza[][] tablero;
 
-    public Tablero(Pieza[][] tablero){
+    public Tablero(){
         this.tablero = new Pieza[8][8];
+        tablero[0][4] = new Rey("Negro", 0, 4);
+        tablero[7][4] = new Rey("Blanco", 7, 4);
     }
 
     public Pieza[][] getTablero() {
@@ -15,5 +18,19 @@ public class Tablero {
 
     public void setTablero(Pieza[][] tablero) {
         this.tablero = tablero;
+    }
+    
+    public void mostrarTablero(){
+        for(int i=0; i<tablero.length; i++){
+            for(int j=0; j< tablero.length; j++){
+                if(tablero[i][j] != null){
+                    System.out.print("[" + tablero[i][j].toString() + "]");
+                }
+                else{
+                    System.out.print("[ ]");
+                }
+            }
+            System.out.println();
+        }
     }
 }
