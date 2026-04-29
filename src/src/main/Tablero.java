837c1647f77c8feafdeea2f54517220904e5ad48
Mario <mariomartinezferreiro@gmail.com>
2026-04-29 13:59:13 +0200

    javadoc (no completo) en Tablero

--- src/src/main/Tablero.java
+++ src/src/main/Tablero.java
@@ -3,9 +3,19 @@ package main;
 import piezas.Pieza;
 import piezas.Rey;
 
+/**
+ * Clase que representa el tablero de ajedrez.
+ * Se encarga de gestionar la disposición de las piezas y la visualización del estado del juego.
+ */
+
 public class Tablero{
+    /** Matriz de 8x8 que contiene las piezas del juego. */
     private Pieza[][] tablero;
 
+    /**
+     * Constructor de la clase Tablero.
+     * Inicializa una matriz de 8x8 y coloca las piezas iniciales en sus posiciones.
+     */
     public Tablero(){
         this.tablero = new Pieza[8][8];
         tablero[0][4] = new Rey("Negro", 0, 4);
@@ -19,7 +29,11 @@ public class Tablero{
     public void setTablero(Pieza[][] tablero) {
         this.tablero = tablero;
     }
-    
+
+    /**
+     * Muestra por consola una representación visual del tablero.
+     * Recorre la matriz e imprime la inicial de la pieza o un hueco vacío si no hay nada.
+     */
     public void mostrarTablero(){
         for(int i=0; i<tablero.length; i++){
             for(int j=0; j< tablero.length; j++){
