1bbc9e9c44d79046d36d28bbf6410e20feff0491
Mario <mariomartinezferreiro@gmail.com>
2026-05-02 20:16:38 +0200

    Clase alfil y pulido del Rey

--- src/src/piezas/Alfil.java
+++ src/src/piezas/Alfil.java
@@ -1,4 +1,29 @@
 package piezas;
 
-public class Alfil {
+import main.Tablero;
+
+/**
+ * Clase que representa la pieza del Alfil
+ */
+public class Alfil extends Pieza {
+
+    public Alfil(String color, int filaActual, int columnaActual){
+        super(color, filaActual, columnaActual);
+    }
+
+    @Override
+    public boolean mover(int filaDestino, int columnaDestino, Tablero tablero){
+        int difFila = Math.abs(filaDestino - getFila());
+        int difColumna = Math.abs(columnaDestino - getColumna());
+
+        // El alfil se mueve en diagonal: la diferencia de filas debe ser igual a la de columnas
+        // También nos aseguramos de que realmente se haya movido (difFila > 0)
+        if(difFila == difColumna && difFila > 0){
+            setFila(filaDestino);
+            setColumna(columnaDestino);
+            return true;
+        }
+
+        return false;
+    }
 }
--- src/src/piezas/Rey.java
+++ src/src/piezas/Rey.java
@@ -1,29 +1,32 @@
 package piezas;
 
 import main.Tablero;
-import interfaces.Movible;
-import piezas.Pieza;
 
 /**
- * Clase que representa la pieza del Rey en el ajedrez.
+ * Clase para la pieza Rey.
  */
-
 public class Rey extends Pieza {
-    public Rey(String color, int filaActual, int columnaActual){
+
+    public Rey(String color, int filaActual, int columnaActual) {
         super(color, filaActual, columnaActual);
     }
 
     @Override
-    public boolean mover(int filaDestino, int columnaDestino, Tablero tablero){
+    public boolean mover(int filaDestino, int columnaDestino, Tablero tablero) {
         int difFila = Math.abs(filaDestino - getFila());
         int difColumna = Math.abs(columnaDestino - getColumna());
 
-        if(difFila <= 1 && difColumna <= 1 && !(difFila == 0 && difColumna == 0)){
+        if (difFila <= 1 && difColumna <= 1 && !(difFila == 0 && difColumna == 0)) {
             setFila(filaDestino);
             setColumna(columnaDestino);
             return true;
         }
 
         return false;
+    }
+
+    @Override
+    public String toString() {
+        return getColor().equalsIgnoreCase("Blanco") ? "RB" : "RN";
     }
 }
