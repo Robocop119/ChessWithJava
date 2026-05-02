8118e65c4a1a9f3d65ed2c79c3d0cb24bd74ccec
Mario <mariomartinezferreiro@gmail.com>
2026-05-02 22:42:40 +0200

    Clase de reina

--- src/src/piezas/Reina.java
+++ src/src/piezas/Reina.java
@@ -1,4 +1,35 @@
 package piezas;
 
-public class Reina {
+import main.Tablero;
+
+/**
+ * Clase para la pieza Reina.
+ */
+public class Reina extends Pieza {
+
+    public Reina(String color, int filaActual, int columnaActual) {
+        super(color, filaActual, columnaActual);
+    }
+
+    @Override
+    public boolean mover(int filaDestino, int columnaDestino, Tablero tablero) {
+        int difFila = Math.abs(filaDestino - getFila());
+        int difColumna = Math.abs(columnaDestino - getColumna());
+
+        // Lógica de Torre (recta) O lógica de Alfil (diagonal)
+        boolean movimientoRecto = (getFila() == filaDestino || getColumna() == columnaDestino);
+        boolean movimientoDiagonal = (difFila == difColumna);
+
+        if ((movimientoRecto || movimientoDiagonal) && difFila + difColumna > 0) {
+            setFila(filaDestino);
+            setColumna(columnaDestino);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return getColor().equalsIgnoreCase("Blanco") ? "QB" : "QN";
+    }
 }
