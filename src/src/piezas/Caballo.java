d65bb57e44fcf94619d81ce5b8ce6b0d85c64dd9
Mario <mariomartinezferreiro@gmail.com>
2026-05-03 18:59:15 +0200

    Clase del caballo

--- src/src/piezas/Caballo.java
+++ src/src/piezas/Caballo.java
@@ -1,4 +1,35 @@
 package piezas;
 
-public class Caballo {
+import main.Tablero;
+
+/**
+ * Clase para la pieza Caballo.
+ */
+public class Caballo extends Pieza {
+
+    public Caballo(String color, int filaActual, int columnaActual) {
+        super(color, filaActual, columnaActual);
+    }
+
+    @Override
+    public boolean mover(int filaDestino, int columnaDestino, Tablero tablero) {
+        int difFila = Math.abs(filaDestino - getFila());
+        int difColumna = Math.abs(columnaDestino - getColumna());
+
+        // Movimiento en L: (2 en uno y 1 en otro) o viceversa
+        boolean movimientoL = (difFila == 2 && difColumna == 1) || (difFila == 1 && difColumna == 2);
+
+        if (movimientoL) {
+            setFila(filaDestino);
+            setColumna(columnaDestino);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return getColor().equalsIgnoreCase("Blanco") ? "CB" : "CN";
+    }
 }
