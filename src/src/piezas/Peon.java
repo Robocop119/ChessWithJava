3143a1cfb9b09d58c4e37d0011af4e2287c5c4a7
Mario <mariomartinezferreiro@gmail.com>
2026-05-05 19:17:17 +0200

    Clases de Peon y Torre

--- src/src/piezas/Peon.java
+++ src/src/piezas/Peon.java
@@ -1,4 +1,44 @@
 package piezas;
 
-public class Peon {
+import main.Tablero;
+
+/**
+ * Clase para la pieza Peon.
+ */
+public class Peon extends Pieza {
+
+    public Peon(String color, int filaActual, int columnaActual) {
+        super(color, filaActual, columnaActual);
+    }
+
+    @Override
+    public boolean mover(int filaDestino, int columnaDestino, Tablero tablero) {
+        int difFila = filaDestino - getFila();
+        int difColumna = Math.abs(columnaDestino - getColumna());
+
+        // Determinamos la dirección según el color
+        int direccion = getColor().equalsIgnoreCase("Blanco") ? -1 : 1;
+        int filaInicio = getColor().equalsIgnoreCase("Blanco") ? 6 : 1;
+
+        // Avance de 1 casilla
+        if (difColumna == 0 && difFila == direccion) {
+            setFila(filaDestino);
+            setColumna(columnaDestino);
+            return true;
+        }
+
+        // Avance inicial de 2 casillas
+        if (difColumna == 0 && getFila() == filaInicio && difFila == 2 * direccion) {
+            setFila(filaDestino);
+            setColumna(columnaDestino);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return getColor().equalsIgnoreCase("Blanco") ? "PB" : "PN";
+    }
 }
--- src/src/piezas/Torre.java
+++ src/src/piezas/Torre.java
@@ -1,4 +1,35 @@
 package piezas;
 
-public class Torre {
+import main.Tablero;
+
+/**
+ * Clase para la pieza Torre.
+ */
+public class Torre extends Pieza {
+
+    public Torre(String color, int filaActual, int columnaActual) {
+        super(color, filaActual, columnaActual);
+    }
+
+    @Override
+    public boolean mover(int filaDestino, int columnaDestino, Tablero tablero) {
+        int difFila = Math.abs(filaDestino - getFila());
+        int difColumna = Math.abs(columnaDestino - getColumna());
+
+        // Movimiento rectilíneo: o cambia la fila o cambia la columna
+        boolean esRecto = (getFila() == filaDestino || getColumna() == columnaDestino);
+
+        if (esRecto && (difFila + difColumna > 0)) {
+            setFila(filaDestino);
+            setColumna(columnaDestino);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return getColor().equalsIgnoreCase("Blanco") ? "TB" : "TN";
+    }
 }
