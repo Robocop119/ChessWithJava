44516b025006dbcc77026154c800ff5a32566351
Mario <mariomartinezferreiro@gmail.com>
2026-05-06 16:35:40 +0200

    terminar reina

--- src/src/piezas/Reina.java
+++ src/src/piezas/Reina.java
@@ -21,10 +21,40 @@ public class Reina extends Pieza {
         boolean movimientoDiagonal = (difFila == difColumna);
 
         if ((movimientoRecto || movimientoDiagonal) && difFila + difColumna > 0) {
+
+            // 1. Determinar la dirección del paso en filas y columnas (-1, 0 o 1)
+            int pasoFila = 0;
+            if (filaDestino > getFila()) pasoFila = 1;
+            else if (filaDestino < getFila()) pasoFila = -1;
+
+            int pasoColumna = 0;
+            if (columnaDestino > getColumna()) pasoColumna = 1;
+            else if (columnaDestino < getColumna()) pasoColumna = -1;
+
+            int filaAux = getFila() + pasoFila;
+            int colAux = getColumna() + pasoColumna;
+
+            // 2. Comprobar que el camino esté despejado hasta antes del destino
+            while (filaAux != filaDestino || colAux != columnaDestino) {
+                if (tablero.getTablero()[filaAux][colAux] != null) {
+                    return false; // El camino está bloqueado
+                }
+                filaAux += pasoFila;
+                colAux += pasoColumna;
+            }
+
+            // 3. Comprobar que en el destino no haya una pieza del mismo color
+            Pieza piezaDestino = tablero.getTablero()[filaDestino][columnaDestino];
+            if (piezaDestino != null && piezaDestino.getColor().equalsIgnoreCase(this.getColor())) {
+                return false; // Fuego amigo
+            }
+
+            // Si pasa todas las validaciones, ejecutamos el movimiento
             setFila(filaDestino);
             setColumna(columnaDestino);
             return true;
         }
+
         return false;
     }
 
