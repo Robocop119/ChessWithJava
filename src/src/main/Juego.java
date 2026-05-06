5872eb64e6dfacdca338ba14b7720b4546e1a86a
Mario <mariomartinezferreiro@gmail.com>
2026-05-06 14:00:47 +0200

    Clase de juego

--- src/src/main/Juego.java
+++ src/src/main/Juego.java
@@ -1,4 +1,26 @@
 package main;
 
+import java.util.Scanner;
+
 public class Juego {
+    private Tablero tablero;
+    private String turnoActual; // "Blanco" o "Negro"
+
+    public Juego() {
+        this.tablero = new Tablero();
+        this.turnoActual = "Blanco";
+    }
+
+    public void iniciar() {
+        Scanner sc = new Scanner(System.in);
+        while (true) { // Bucle principal del juego
+            tablero.mostrarTablero();
+            System.out.println("Turno de: " + turnoActual);
+
+            // 1. Pedir coordenadas (Ej: 6 4 a 4 4)
+            // 2. Buscar la pieza en el origen
+            // 3. Intentar mover
+            // 4. Si el movimiento es válido, cambiar el turno
+        }
+    }
 }
