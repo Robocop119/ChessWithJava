109e47f222a43c6ebc62dba4149ffe4434808882
Mario <mariomartinezferreiro@gmail.com>
2026-05-06 15:51:20 +0200

    clase jugador

--- src/src/main/Jugador.java
+++ src/src/main/Jugador.java
@@ -1,4 +1,22 @@
 package main;
 
+/**
+ * Clase que representa a un jugador de la partida.
+ */
 public class Jugador {
+    private String nombre;
+    private String color;
+
+    public Jugador(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
 }
